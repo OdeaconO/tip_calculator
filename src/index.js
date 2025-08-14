4b0c613ff597fe415063967ac0b51e52c420e3fa
OdeaconO <dhruvrai2903@gmail.com>
2025-08-15 05:01:24 +0530

    changes related to pwa

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Import the service worker register function
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Register the service worker so the app works offline and loads faster
+serviceWorkerRegistration.register();
