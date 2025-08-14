88a60a69614e400186a6a15ae0cfb3d16677f37f
OdeaconO <dhruvrai2903@gmail.com>
2025-08-15 04:11:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [service1, setService1] = useState(0);
+  const [service2, setService2] = useState(0);
+
+  function resetValues() {
+    setBill(0);
+    setService1(0);
+    setService2(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BillAmount bill={bill} setBill={setBill}></BillAmount>
+      <Service service={service1} setService={setService1}>
+        How did you like the service?
+      </Service>
+      <Service service={service2} setService={setService2}>
+        How did your friends like the service?
+      </Service>
+      <TotalAmount
+        bill={bill}
+        service1={service1}
+        service2={service2}
+      ></TotalAmount>
+      <ResetButton onResetValues={resetValues}>Reset</ResetButton>
     </div>
   );
 }
 
-export default App;
+function BillAmount({ bill, setBill }) {
+  return (
+    <div>
+      <h3>How much was the bill?</h3>
+      <form>
+        <input
+          type="number"
+          min="0"
+          step="any"
+          value={bill}
+          onChange={(e) => {
+            const val = Math.max(0, Number(e.target.value));
+            setBill(val);
+          }}
+        ></input>
+      </form>
+    </div>
+  );
+}
+
+function Service({ service, setService, children }) {
+  return (
+    <div>
+      <h3>{children}</h3>
+      <select
+        value={service}
+        onChange={(e) => setService(Number(e.target.value))}
+      >
+        <option value={0}>Dissatisfied (0%)</option>
+        <option value={5}>It was okay (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function TotalAmount({ bill, service1, service2 }) {
+  const avgTip = (service1 + service2) / 2;
+  const tip = (bill * avgTip) / 100;
+  const TotalAmount = bill + tip;
+
+  return (
+    <h2>
+      You pay ${TotalAmount} (${bill} + ${tip})
+    </h2>
+  );
+}
+
+function ResetButton({ onResetValues, children }) {
+  return <button onClick={() => onResetValues()}>{children}</button>;
+}
